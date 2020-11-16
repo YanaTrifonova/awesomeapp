27ecc0f0a344f9acf4749e2b6dd91e6fe3a80c33
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-11-16 16:26:49 +0100

    added pokemon component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import logo from './logo.svg';
+import Pokemon from "./Pokemon"
+
 import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world!
-        </a>
-        <p>Welcome to Codaisseur.</p>
-      </header>
-      <h1>Wow!</h1>
-    </div>
-  );
+    const pokemonData = [
+        {
+            name: "Charizard",
+            weight: "90",
+            awesome: true,
+            terrifying: false,
+            abilities: ["Blaze", "Solar power", "Tough claws", 'Drought']
+
+        },
+        {
+            name: "Bulbasaur",
+            weight: "6.9",
+            awesome: true,
+            terrifying: false,
+            abilities: ["Overgrow", "Chlorophyll"]
+
+        },
+        {
+            name: "Mewtwo",
+            weight: "122",
+            awesome: true,
+            terrifying: true,
+            abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+
+        },
+        {
+            name: "Mega beedrill",
+            weight: "65",
+            awesome: false,
+            terrifying: true,
+            abilities: ["Intimidate", "Unnerve"]
+
+        },
+    ]
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Pokemon's cards</h1>
+                <main>
+                    {pokemonData.map((pokemon) => {
+                        return <Pokemon name={pokemon.name} weight={pokemon.weight} awesome={pokemon.awesome}
+                                        terrifying={pokemon.terrifying} abilities={pokemon.abilities}/>
+                    })}
+                </main>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
